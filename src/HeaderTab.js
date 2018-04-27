2840d3c067853e78a5611e9c56831af15be89c0a
gfridh <fridh.g@gmail.com>
2018-04-27 02:46:21 +0200

    stuff

--- src/HeaderTab.js
+++ src/HeaderTab.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class HeaderTab extends Component {
   render() {
     return (
-      <div className="HeaderTab">
+      <div className="HeaderTab" style={{cursor: "pointer"}}>
         <p>{this.props.title}</p>
       </div>
     );
